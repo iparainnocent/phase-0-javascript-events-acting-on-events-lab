e6eb045016cc1d013fc938b7a7c8a7bb8bde7435
iparainnocent <innocent.ipara@student.moringaschool.com>
2024-07-08 12:09:39 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+let dodgerElement = document.getElementById('dodger');
+let dodgerLeftEdge = 0;
+
+function moveDodgerLeft() {
+  dodgerLeftEdge -= 10;
+  setDodgerPosition();
+}
+
+function moveDodgerRight() {
+  dodgerLeftEdge += 10;
+  setDodgerPosition();
+}
+
+function setDodgerPosition() {
+  dodgerElement.style.left = dodgerLeftEdge + 'px';
+}
